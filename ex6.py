f5ff5730b19e8fd0b6843e63bfcf8040b883f91c
Jokertion <645434934@qq.com>
2018-04-07 09:06:02 +0800

    练习6 加注释版

--- ex6.py
+++ ex6.py
@@ -1,20 +1,20 @@
-x = "There are %d types of people." % 10    #%d 数字格式化
-binary = "binary"							#赋值binary 字符串	 
-do_not = "don't" 							#赋值do_not 字符串
+x = "There are %d types of people." % 10    # %d 数字格式化
+binary = "binary"							# 赋值binary 字符串	 
+do_not = "don't" 							# 赋值do_not 字符串
 y = "Those who know %s and those who %s."% (binary,do_not)  #多个格式化，后加逗号
 
-print (x) 									#输出字符串	
-print (y)									#输出字符串
+print (x) 									# 输出字符串	
+print (y)									# 输出字符串
 
-print ("I said: %r." % x)					#%r 显示原始数据值
-print ("I also said: '%s'. " % y)			#%s 显示输出的
+print ("I said: %r." % x)					# %r 显示原始数据值
+print ("I also said: '%s'. " % y)			# %s 显示输出的
 
-hilarious = False 							#hilarious 赋布尔值 
+hilarious = False 							# hilarious 赋布尔值 
 joke_evaluation = "Isn't that joke so funny?! %r"  #joke_evaluation赋字符串，%r占位
 
-print (joke_evaluation % hilarious)			#输出 joke_evaluation， hilarious赋值%r
+print (joke_evaluation % hilarious)			# 输出 joke_evaluation， hilarious赋值%r
 
-w = "This is the left side of..."			#赋值w
-e = "a sting with a right side."			#赋值e
+w = "This is the left side of..."			# 赋值w
+e = "a sting with a right side."			# 赋值e
 
-print (w + e) 								#输出 w+e
+print (w + e) 								# 输出 w+e
