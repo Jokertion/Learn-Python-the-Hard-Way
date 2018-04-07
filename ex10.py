4aa7689c47a96c32d18ffd32a6f111201e62efd3
Jokertion <645434934@qq.com>
2018-04-08 00:03:43 +0800

    练习10

---
+++ ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbled in."   		#\t 制表符
+persian_cat = "I'm split\non a line."   #\n 换行
+backslash_cat = "I'm \\ a \\ cat."		#\\ 反斜杠
+										#三引号 放入多行文
+fat_cat = """						 
+I'll do a list:
+\t* Cat food 
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
