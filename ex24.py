d1842a8d481bbf9c9af6fbdc8f53b500d4c5a27b
Jokertion <645434934@qq.com>
2018-04-13 21:45:02 +0800

    练习24 注释版

--- ex24.py
+++ ex24.py
@@ -1,6 +1,8 @@
 print ("Let's practice everything.")
+#转义字符 \n换行 \t横向制表符
 print ('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
-	
+
+#三引号 放入多行文字	
 poem = """
 \tThe lovely world
 with logic so firmly planted
@@ -14,9 +16,11 @@ print ("---------------")
 print (poem)
 print ("---------------")
 
+#格式化 调用数字
 five = 10 - 2 + 3 - 6
 print ("This should be five: %s" % five)
 
+#定义方法：最初值
 def secret_formula(started):
 	jelly_beans = started * 500
 	jars = jelly_beans / 1000
@@ -26,10 +30,12 @@ def secret_formula(started):
 start_point  = 10000
 beans, jars, crates = secret_formula(start_point)
 
+#格式化 调用参数
 print ("With a starting point of: %d" % start_point)
 print ("We'd have %d beans, %d jars, and %d crates."
 	% (beans, jars, crates))
 	
+#格式化 调用函数
 start_point = start_point / 10
 print ("We can also do that this way:")
 print ("We'd have %d beans, %d jars, and %d crates."
