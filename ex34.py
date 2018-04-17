256861dd9a267cbafad3d7a30795a1f06fa4114f
Jokertion <645434934@qq.com>
2018-04-17 14:03:23 +0800

    练习34

---
+++ ex34.py
@@ -0,0 +1,16 @@
+#练习34.访问列表元素
+#序数(ordinal number)--基数(cardinal number)
+#ordinal == 有序，以 1 开始
+#cardinal == 随机选取, 以 0 开始。
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+#The 1st animal is at 0 and is a bear.”
+#The animal at 0 is the 1st animal and is a bear.”
+The animal at 1. 		# python
+The third (3rd) animal. # peacock
+The first (1st) animal. # bear
+The animal at 3.		# kangaroo
+The fifth (5th) animal.	# whale
+The animal at 2.		# peacock
+The sixth (6th) animal.	# platypus
+The animal at 4.		# whale
