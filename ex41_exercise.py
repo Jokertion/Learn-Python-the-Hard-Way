5da3fd4b2a593c504b54681e5d51780c7626cd36
Jokertion <645434934@qq.com>
2018-04-20 11:44:33 +0800

    练习41_面向对象练习题

---
+++ ex41_exercise.py
@@ -0,0 +1,43 @@
+#面向对象 短语解释文摘及练习
+"""
+创建一个叫X的类，并继承Y。
+class X(Y):
+
+类X有一个__init__方法，该方法有self和J两个参数。
+class X(object):
+	def __init__(self, J):
+
+类X有一个叫M的函数，该函数有self和J两个参数。
+class X(object):
+	def M(self, J):
+
+给foo赋值为类X的一个实例。
+foo = X()
+
+从foo里调用M函数，传递的参数为self和J。
+foo.M(J)
+		
+从foo里调用K属性，并将其设置为Q。
+foo.K = Q
+"""
+
+#1.创建一个叫？？的类继承Y  
+#	class ??(Y)
+
+#2.类？？有一个__init__方法，该方法有self和？？两个参数。
+#	class ??(object):
+#		def __init__(self, ??):
+
+#3.类？？有一个叫？？的函数，该函数有self和？？两个参数。
+#	class ??(object):
+#		def ??(self, ??):
+
+#4.给foo赋值为类？？的一个实例。
+#	foo = ??()
+	
+#5.从foo里调用？？函数，传递的参数为self和？？。
+#	foo.??(??)
+
+
+#6.从foo里调用？？属性，并将其设置为？？。
+#	foo.?? = ??
